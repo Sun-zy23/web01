65de220c224b7ab98e0d6b8cc62148afa9c65f1d
你注册GitHub的用户名 <GitHub>
2020-11-25 09:25:22 +0800

    第十八次课

---
+++ 第十八次课/js函数2.js
@@ -0,0 +1,99 @@
+
+
+// 变量
+
+// int apple = 20
+
+// var apple = 20;
+//  var orange = "橘子";
+
+
+//  es6
+//  let lemon = "柠檬";
+//  let banana = 30;
+//  const pear = 40;
+
+//在js里面一切都是对象
+
+//var apple1 = {}
+
+//console.log(typeof apple1)
+
+
+//var apple2 =Object.create();
+
+//console.log(typeof apple2 + "apple2");
+
+
+// var apple3 = function bigApple() {
+//     console.log("this is apple3 obig apple");
+// }
+// function smallApple(){
+//     console.log("this is small apple")
+// }
+// apple3()
+// bigApple()
+// smallApple()
+// var apple4 = function () {
+//     console.log("this is apple3 big apple");
+// }
+
+// apple4()
+
+// var apple5 = () =>{
+//     console.log("this is arrow fuction");
+// }
+// apple5()
+
+// var apple6 = () =>{
+//     console.log("this is arrow fuction");
+// }
+// apple6()
+
+
+// var apple7 = ()=>{
+//     return ()=>{
+//         console.log("this is arrow fuction apple7");
+//     }
+// }
+// apple7()();
+
+// var apple10 =(x,y)=>{
+//    console.log("this is apple 10 == "+(x+y));
+//    return (x,y)=>{
+//     console.log("this is apple 11 == "+(x+y));
+//    }
+// }
+// //NaN   not a number
+// apple10(4,5)(8,9)
+
+//  var apple12 = () => console.log("10");console.log("13")
+// apple12()
+
+// function  apple13 (){
+//     var name ="苹果13";
+//     function apple13Son (){
+//         console.log("this is apple13Son")
+//     }
+//     apple13Son()
+//     console.log("this is apple13")
+//     return {
+//         apple13Son1:apple13Son
+//     }
+// }
+// apple13().apple13Son1();
+
+// var apple14 =function (){
+//     console.log("this is apple14 ...")
+//     return 100;
+// }
+
+// var apple14 =temp1()
+
+// console.log(apple14);
+
+var result = function(){
+    console.log("this is apple15")
+    return 200;
+}()
+// var result = apple15()
---
+++ 第十八次课/tempCodeRunnerFile.js
@@ -0,0 +1,8 @@
+var apple14 =function (){
+//     console.log("this is apple14 ...")
+//     return 100;
+// }
+
+// var apple14 =temp1()
+
+// console.log(apple14);
